835deb038551071e1f8324599d0c58f34b79a6f0
Sir-Deon <sird655@gmail.com>
2021-10-02 21:37:24 +0100

    deploying

--- apis/user.js
+++ apis/user.js
@@ -10,15 +10,13 @@ const {
   auth_desktop,
 } = require("../controller/user");
 
-router.get("/", (req, res) => [res.send("hello ")]);
+router.get("/", (req, res) => [res.send("hello there")]);
 
 router.get("/auth_desk/:code", (req, res) => [auth_desktop(req, res)]);
 
 router.get("/get_sites", (req, res) => [getSites(req, res)]);
 
-router.post("/sign_up", (req, res) => {
-  console.log("Heyyy");
-});
+router.post("/sign_up", (req, res) => [signup(req, res)]);
 router.post("/login", (req, res) => [login(req, res)]);
 
 module.exports = router;
